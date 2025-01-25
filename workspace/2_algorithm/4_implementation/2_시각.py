01644950ef1754247b77569e0c0a24fa14eff4b7
HaYoung Kang <118874524+pulpo125@users.noreply.github.com>
2025-01-25 05:16:53 +0000

    증간저장

---
+++ workspace/2_algorithm/4_implementation/2_시각.py
@@ -0,0 +1,21 @@
+"""
+# 구현
+## 예제 4-2) 시각
+- 113p
+
+정수 N이 입력되면 0시0분0초부터 N시59분59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수를 구하는 프로그램을 작성해라.
+"""
+from src.utils import timer
+
+@timer
+def solution(n: int):
+    pass
+
+
+if __name__ == "__main__":
+    # 입력
+    n = int(input())
+
+    # 실행
+    result = solution(n)
+    print(result)
