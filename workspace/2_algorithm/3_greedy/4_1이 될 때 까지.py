d9c556b58ac350c46a72c997c9374ca8077a827b
HaYoung Kang <118874524+pulpo125@users.noreply.github.com>
2025-01-25 04:44:29 +0000

    solve again: 그리디_1이 될 때 까지

--- workspace/2_algorithm/3_greedy/4_1이 될 때 까지.py
+++ workspace/2_algorithm/3_greedy/4_1이 될 때 까지.py
@@ -2,18 +2,29 @@
 # 그리디
 ## 1이 될 때까지
 - 99p
+
+n이 1이 될 때까지 다음 두 과정 중 하나를 반복적으로 선택하여 수행.
+1. n-1
+2. n / k (나누어 떨어지는 경우에만 수행 가능)
 """
 from src.utils import timer
 
 @timer
 def solution(n, k):
     cnt = 0
-    while n != 1:
-        if n % k == 0:
-            n = n / k
+    while True:
+        # 나누어 떨어지는 수 만들기
+        target_num = (n // k) * k
+        cnt += n - target_num
+        n = target_num 
+
+        if n < k:
+            break
         else:
-            n = n - 1
-        cnt += 1
+            # 1이 될때까지 나누기
+            cnt += 1
+            n //= k
+            print(n)
     return cnt
 
 if __name__ == "__main__":
@@ -23,5 +34,5 @@ if __name__ == "__main__":
 
     # 실행
     # result = solution(n, k)
-    result = solution(25, 5)
+    result = solution(25, 3)
     print(result)
