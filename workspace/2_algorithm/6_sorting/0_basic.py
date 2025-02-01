a79ef99ce20c4a8824449cc120e923eb30946f3e
HaYoung Kang <118874524+pulpo125@users.noreply.github.com>
2025-02-01 09:31:47 +0000

    퀵 정렬 공부

--- workspace/2_algorithm/6_sorting/0_basic.py
+++ workspace/2_algorithm/6_sorting/0_basic.py
@@ -18,7 +18,7 @@ def selection_sort(array: list) -> list:
 2. 삽입 정렬
 - 삽입 정렬은 두 번째 원소(인덱스 1) 부터 왼쪽은 정렬되었다는 가정하에 왼쪽 리스트에 올바른 위치에 삽입을 해서 점차 정렬해나가는 정렬 방법이다.
 - 왼쪽 리스트에서 더 작은 원소를 만나면 그 자리에 삽입 되면 된다. 
-- 시간 복잡도는 O(N2)으로 선택 정렬과 같지만, 이미 어느정도 정렬이 된 리스트에서는 퀵 정렬보다 빠른 속도이다.
+- 시간 복잡도는 최악에 경우에는 O(N2)으로 선택 정렬과 같지만, 이미 어느정도 정렬이 된 리스트에서는 O(N)으로 퀵 정렬보다 빠른 속도이다.
 """
 
 def insertion_sort(array: list) -> list:
@@ -30,6 +30,71 @@ def insertion_sort(array: list) -> list:
                 break
     return array
 
+"""
+3. 퀵 정렬
+- 퀵 정렬은 피벗이라는 기준점을 정하고 왼쪽에서 부터 피벗보다 큰 데이터를 찾고, 오른쪽에서 부터는 피벗보다 작은 데이터를 찾아 두 수의 위치를 변경하여,
+  결국 피벗 왼쪽에는 피벗보다 작은 수를, 오른쪽에는 피벗보다 큰 수를 위치시켜 계속 퀵 정렬을 해나가는 정렬 방법이다. 
+- 만약, 왼쪽에 작은 수가 있고 오른쪽에 큰 수가 있으면 작은 수와 피벗의 위치를 변경시켜 피벗 왼쪽 리스트, 오른쪽 리스트를 각각 퀵 정렬을 다시 수행한다.
+- 시간 복잡도는 O(nlogn) 이다.
+"""
+
+def sorted_quick(array: list, start: int, end: int) -> list:
+
+    # 원소가 한 개인 경우 종료
+    if start >= end:
+        return array
+    
+    # pivot, left, right
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+        
+        # pivot 보다 큰 left 찾기
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        
+        # pivot 보다 작은 right 찾기
+        while right > start and array[right] >= array[pivot]:
+            right += -1
+        
+        # 만약 작은 수와 큰 수의 위치가 바뀌었으면 pivot 과 작은 수의 위치를 바꾼다.
+        if left > right:
+            array[pivot], array[right] = array[right], array[pivot]
+        else:
+            # 만약 작은 수, 큰 수 위치가 바뀌지 않았다면 두 수를 바꾼다.
+            array[left], array[right] = array[right], array[left]
+        
+        # 재귀로 윈쪽, 오른쪽 리스트 다시 퀵 정렬 시행
+        sorted_quick(array, start, right-1)
+        sorted_quick(array, right+1, end)
+    
+    return array
+
+def light_sorted_quick(array: list) -> list:
+
+    # 만약 리스트 원소가 한개면 종료
+    if len(array) <= 1:
+        return array
+    
+    # pivot, 정렬을 수행할 리스트
+    pivot = array[0]
+    processing_list = array[1:]
+
+    left_list = [i for i in processing_list if pivot >= i]
+    right_list = [i for i in processing_list if pivot < i]
+
+    # 왼쪽 오른쪽 리스트에 대해서도 퀵 정렬 수행
+    return light_sorted_quick(left_list) + [pivot] + light_sorted_quick(right_list)
+    
+
+def quick_sort(array: list) -> list:
+    # return sorted_quick(array, 0, len(array)-1)
+    return light_sorted_quick(array)
+
+
+
 if __name__ == "__main__":
     array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
     selection_sorted_array = selection_sort(array)
@@ -37,4 +102,7 @@ if __name__ == "__main__":
 
     insertion_sorted_array = insertion_sort(array)
     print(f"삽입 정렬 결과: {insertion_sorted_array}")
+
+    quick_sorted_array = quick_sort(array)
+    print(f"퀵 정렬 결과: {quick_sorted_array}")
 
